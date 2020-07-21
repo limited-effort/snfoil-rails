771c26b23bd8461496a2770d8ef8380873f58fde
Matthew Howes <howeszy@gmail.com>
2020-07-21 19:42:59 -0400

    Add specs

---
+++ spec/sn_foil/adapters/orms/active_record_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SnFoil::Adapters::ORMs::ActiveRecord do # rubocop:disable RSpec/FilePath
+  it 'doesn\'t raise an association error with an ActiveRecord Model' do
+    wrapped_class = described_class.new(Person).new
+    expect(wrapped_class.is_a?(Person)).to be true
+  end
+end
