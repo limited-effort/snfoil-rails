996ffb28833b341674e3015aed285500c69bd8b9
Matthew Howes <howeszy@gmail.com>
2021-09-14 17:01:16 -0400

    Fix JSONAPI Serializer break

--- lib/sn_foil/jsonapi_serializer.rb
+++ lib/sn_foil/jsonapi_serializer.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
-require 'fast_jsonapi/object_serializer'
+require 'jsonapi/serializer'
 
 module SnFoil
   module JsonapiSerializer
     extend ActiveSupport::Concern
 
     included do
-      include FastJsonapi::ObjectSerializer
+      include JSONAPI::Serializer
 
       set_key_transform :dash
     end
