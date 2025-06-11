359421c5906bcaecd92ab75466e86195c68acba7
Cliff Campbell <cliff.campbell@crakn.net>
2025-06-11 09:30:43 -0400

    Adjust module name to match directory structure (sn_foil vs snfoil)

--- lib/generators/snfoil/jsonapi_serializer/jsonapi_serializer_generator.rb
+++ lib/generators/snfoil/jsonapi_serializer/jsonapi_serializer_generator.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-module SnFoil
+module Snfoil
   class JsonapiSerializerGenerator < ::Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
